82a378287cdee039cbf8d283c45b1c4f1d3afb6e
Mariah Carey <219577531+xXMariahScaryXx@users.noreply.github.com>
2026-01-19 23:02:24 +0000

    more

--- shim_utils.sh
+++ shim_utils.sh
@@ -19,14 +19,14 @@ extract_initramfs() {
   #extract the compressed kernel image from the partition data
   local kernel_file="$(basename $kernel_bin)"
   local binwalk_out=$(run_binwalk --extract $kernel_bin --directory=$working_dir)
-  local stage1_file=$(echo $binwalk_out | pcregrep -o1 "\d+\s+0x([0-9A-F]+)\s+gzip compressed data")
+  local stage1_file=$(echo $binwalk_out | pcre2grep -o1 "\d+\s+0x([0-9A-F]+)\s+gzip compressed data")
   local stage1_dir="$working_dir/_$kernel_file.extracted"
   local stage1_path="$stage1_dir/$stage1_file"
   
   #extract the initramfs cpio archive from the kernel image
   run_binwalk --extract $stage1_path --directory=$stage1_dir > /dev/null
   local stage2_dir="$stage1_dir/_$stage1_file.extracted/"
-  local cpio_file=$(file $stage2_dir/* | pcregrep -o1 "([0-9A-F]+):\s+ASCII cpio archive")
+  local cpio_file=$(file $stage2_dir/* | pcre2grep -o1 "([0-9A-F]+):\s+ASCII cpio archive")
   local cpio_path="$stage2_dir/$cpio_file"
 
   rm -rf $output_dir
@@ -40,7 +40,7 @@ extract_initramfs_arm() {
 
   #extract the kernel lz4 archive from the partition
   local binwalk_out="$(run_binwalk $kernel_bin)"
-  local lz4_offset="$(echo "$binwalk_out" | pcregrep -o1 "(\d+).+?LZ4 compressed data" | head -n1)"
+  local lz4_offset="$(echo "$binwalk_out" | pcre2grep -o1 "(\d+).+?LZ4 compressed data" | head -n1)"
   local lz4_file="$working_dir/kernel.lz4"
   local kernel_img="$working_dir/kernel_decompressed.bin"
   dd if=$kernel_bin of=$lz4_file iflag=skip_bytes,count_bytes skip=$lz4_offset
@@ -49,7 +49,7 @@ extract_initramfs_arm() {
   #extract the initramfs cpio archive from the kernel image
   local extracted_dir="$working_dir/_kernel_decompressed.bin.extracted"
   run_binwalk --extract $kernel_img --directory=$working_dir > /dev/null
-  local cpio_file=$(file $extracted_dir/* | pcregrep -o1 "([0-9A-F]+):\s+ASCII cpio archive")
+  local cpio_file=$(file $extracted_dir/* | pcre2grep -o1 "([0-9A-F]+):\s+ASCII cpio archive")
   local cpio_path="$extracted_dir/$cpio_file"
 
   rm -rf $output_dir
